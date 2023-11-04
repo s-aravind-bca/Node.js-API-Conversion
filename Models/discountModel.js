023aaf4f4c3961ffe9c4807a880e24a6edf6f4cb
Aravind <147846043+s-aravind-bca@users.noreply.github.com>
2023-11-04 14:48:46 +0530

    Node.js API conversion

---
+++ Models/discountModel.js
@@ -0,0 +1,18 @@
+import { Double } from "mongodb";
+import mongoose from "mongoose";
+const discountSchema = new mongoose.Schema({
+    product:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    Discount:{
+        type:Number,
+        required:true
+    },
+})
+
+export default mongoose.model('discount',discountSchema)
